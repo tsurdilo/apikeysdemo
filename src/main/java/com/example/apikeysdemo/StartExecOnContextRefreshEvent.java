d9c5bee19c815f81648f96330839da09c432d8cd
Tihomir Surdilovic <tihomir@temporal.io>
2025-04-16 23:43:08 -0400

    added multins support
    Signed-off-by: Tihomir Surdilovic <tihomir@temporal.io>

--- src/main/java/com/example/apikeysdemo/StartExecOnContextRefreshEvent.java
+++ src/main/java/com/example/apikeysdemo/StartExecOnContextRefreshEvent.java
@@ -1,9 +1,10 @@
 package com.example.apikeysdemo;
 
 import com.example.apikeysdemo.workflows.MyWorkflow;
+import com.example.apikeysdemo.workflows2.MyWorkflowTwo;
 import io.temporal.client.WorkflowClient;
 import io.temporal.client.WorkflowOptions;
-import org.springframework.beans.factory.annotation.Autowired;
+import jakarta.annotation.Resource;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -11,19 +12,31 @@ import org.springframework.stereotype.Component;
 @Component
 public class StartExecOnContextRefreshEvent {
 
-    @Autowired
+    @Resource
     private WorkflowClient workflowClient;
+
+    @Resource(name = "secondWorkflowClient")
+    private WorkflowClient secondWorkflowClient;
 
 
     @EventListener
     public void onApplicationEvent(ContextRefreshedEvent event) {
         MyWorkflow workflow = workflowClient.newWorkflowStub(MyWorkflow.class,
                 WorkflowOptions.newBuilder()
-                        .setWorkflowId("my-workflow")
+                        .setWorkflowId("my-workflow-new")
                         .setTaskQueue("mytaskqueue")
                         .build());
 
         // start async
         WorkflowClient.start(workflow::sayHello, "Michael Jackson");
+
+        MyWorkflowTwo workflowTwo = secondWorkflowClient.newWorkflowStub(MyWorkflowTwo.class,
+                WorkflowOptions.newBuilder()
+                        .setWorkflowId("my-workflow-new-two")
+                        .setTaskQueue("mytaskqueue")
+                        .build());
+
+        // start async
+        WorkflowClient.start(workflowTwo::sayHello, "Michael Jordan");
     }
 }
--- src/main/java/com/example/apikeysdemo/workflows/MyWorkflowImpl.java
+++ src/main/java/com/example/apikeysdemo/workflows/MyWorkflowImpl.java
@@ -2,13 +2,9 @@ package com.example.apikeysdemo.workflows;
 
 import io.temporal.spring.boot.WorkflowImpl;
 import io.temporal.workflow.Workflow;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Profile;
 
 import java.time.Duration;
 
-//@Profile("one")
-//@ComponentScan
 @WorkflowImpl(taskQueues = "mytaskqueue")
 public class MyWorkflowImpl implements MyWorkflow {
     @Override
